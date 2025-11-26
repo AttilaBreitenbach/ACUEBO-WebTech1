354042fdc9bf3251c3a8700bc76a391cdfbb0d30
AttilaBreitenbach <135427297+AttilaBreitenbach@users.noreply.github.com>
2025-11-26 13:22:20 +0100

    Add files via upload

---
+++ ACUEBO_1126/script.js
@@ -0,0 +1,15 @@
+function Kattint(){
+    document.getElementById("eltunt").style.display = "none";
+}
+function DoubleClick(){
+    document.getElementById("double").style.display="none";
+}
+function borderColor(){
+    document.getElementById("mail").style.borderBlockColor="red";
+}
+function alap(){
+    document.getElementById("mail").style.borderBlockColor="black";
+}
+function kitolt(){
+    document.getElementById("pasw").style.backgroundColor="blue";
+}
