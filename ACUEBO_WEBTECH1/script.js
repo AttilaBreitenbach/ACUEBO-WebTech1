aaf272c8e6ff8419f3745f3196dcd0a1d8572c32
AttilaBreitenbach <135427297+AttilaBreitenbach@users.noreply.github.com>
2025-11-29 18:49:11 +0100

    Add files via upload

---
+++ ACUEBO_WEBTECH1/script.js
@@ -0,0 +1,36 @@
+const months = ["Január","Február","Március","Április","Május","Június","Július","Augusztus","Szeptember","Október","November","December"];
+const d = new Date();
+let month = months[d.getMonth()];
+let day = d.getDate();
+document.getElementById("date").innerHTML = month +" "+ day;
+//////////////////////////////////
+var countDownDate = new Date("Dec 06, 2025 00:00:00").getTime();
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+
+  document.getElementById("timer").innerHTML = days + "nap " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+//////////////////////////
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
