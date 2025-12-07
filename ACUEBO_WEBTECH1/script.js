258a7d8dc5ddbff74ac82b60bdf6f33390956fc0
Attila Breitenbach <breitenbach.attila@student.uni-miskolc.hu>
2025-12-07 12:17:33 +0100

    1.8

--- ACUEBO_WEBTECH1/script.js
+++ ACUEBO_WEBTECH1/script.js
@@ -28,11 +28,3 @@ var x = setInterval(function() {
   }
 }, 1000);
 //////////////////////////
-var modal = document.getElementById('id01');
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-////////////////////////////
